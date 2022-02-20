efab7413d39281caf0d035b4a98f4f56c9b32d97
NinjaOla <olamathiesen1@gmail.com>
2022-02-20 22:24:31 +0100

    Add files via upload
    Made changes to structure, added commands and reusability of code.

--- bot.py
+++ bot.py
@@ -8,55 +8,98 @@ import time
 import sys
 
 #VARIABLES
-api_key = 'INSERT API KEY HERE' # here please put in-between the quotes your Riot API Developer Key
+api_key = 'key1' # here please put in-between the quotes your Riot API Developer Key
 watcher = LolWatcher(api_key)
-my_region = 'euw1'
-region = 'europe'
+
 current_time = int(time.time())
 last = (current_time - 86400)
 week = (current_time - 604800)
 two = (current_time - 172800)
-print(current_time)
-print(last)
+teamArr = []
 
-#PUUIDS ----------------------------------
-top_id = 'fQwRacUndeCleiOeQi4qm4WTFuoqSZg7SXnQWeAepWZPZ6teQ7Aytl5wAoAG3ltsM_ZcNT904BbIUg' # here write the puuid of your toplaner
-jgl_id = 'Kae9k0d4l2o6sKblB36Pm-IuPjgDNFLu2Vz0ODNcOG2PaNVXRWL27j82I7-UKGCwDgifz82RrT6zQA' # here write the puuid of your jungler
-mid_id = '4B_9lFf5JU4onY__sQyZY6RVVNrXnsCsX6vqYgjWKjlFqnGj4Vb0gxCa0mUMWU8uK_6NM-IeqaB_ag' # here write the puuid of your midlaner
-adc_id = 'eCze5I2zMSyG4x84qyVrlbJjQOdyr6cn6IPah6AOjpZA7x6LBm4EtdMUJzjR8EAZBazCsGqSy7-oTw' # here write the puuid of your ad carry
-sup_id = 'IQYpa2gn6ocPcdte-LHXNk8VTVSrcFY6EzfJqux4Nw33wonDXugp6bChZm3bY5-hC2d-2g_17HfMGg' # here write the puuid of your support
-team_id = [top_id, jgl_id, mid_id, adc_id, sup_id]
-team_mh = []
-#CALCULS ----------------------------------
-player_name = ['Toplaner', 'Jungler', 'Midlaner', 'ADCarry', 'Support']
-game_nbrs = []
+def regionSelectorSmallBig(regionInput):
+    print("we are in region selector")
+    if "eu" in regionInput:
+        regionInput = "europe"
+    if "na" in regionInput:
+        regionInput = "americas"
+    if "as" in regionInput:
+        regionInput = "asia"
 
-# DAY COMMAND ----------------------------------
-if sys.argv[1] == "day":
-    team_mh = [list(watcher.match.matchlist_by_puuid(region, id, type="ranked", start_time=last, end_time=current_time, count=100)) for id in team_id]
-# WEEK COMMAND ----------------------------------
-if sys.argv[1] == "week":
-    team_mh = [list(watcher.match.matchlist_by_puuid(region, id, type="ranked", start_time=week, end_time=current_time, count=100)) for id in team_id]
+    print("Returning: " + str(regionInput))
+    return regionInput
 
-#LAST TWO DAYS COMMAND ----------------------------------
-if sys.argv[1] == "two":
-    team_mh = [list(watcher.match.matchlist_by_puuid(region, id, type="ranked", start_time=two, end_time=current_time, count=100)) for id in team_id]
+def getSummonerByRegionName(region, name):
+    return watcher.summoner.by_name(region, name)
 
-# API DATAFRAME CREATION ----------------------------------
-game_nbrs = [len(elt) for elt in team_mh]
+def getPuuidBySummonerInternal(summoner):
+    return summoner['puuid']
 
-df = pd.DataFrame(
-    {'Player': player_name,
-    'phrase': "has played",
-    'Games': game_nbrs,
-    'last': "games of soloQ in the last 24 hours.",
-})
+def getNameBySummonerInternal(summoner):
+    return summoner['name']
 
-print(df)
+def getMatchListByRegionAndIdAndTimeFrame(region, puuId, startTime):
 
-i = 0
-df1 = df.loc[[i]]
+    return list(watcher.match.matchlist_by_puuid(region, puuId, type="ranked", start_time=startTime, end_time=current_time, count=100))
 
+def getTimeByStringInternal(input):
+    if input == "1":
+        return last
+
+    if input == "2":
+        return two
+
+    if input == "week":
+        return week
+
+def getTimeStringFromInput(input):
+    if input == "1":
+        return "the last day"
+
+    if input == "2":
+        return "the last two days"
+
+    if input == "week":
+        return "the last week"
+
+def addPlayerToTeamInternal(region, name):
+    playervalues = createPlayerKeyInternal(region,name)
+    
+    if playervalues not in teamArr:
+        teamArr.append(playervalues)
+        return True
+    return False
+
+def removePlayerFromTeamInternal(region, name):
+    playervalues = createPlayerKeyInternal(region,name)
+    
+    if playervalues in teamArr:
+        teamArr.remove(playervalues)
+        return True
+    return False
+
+def listTeamInternal():
+    returnValue = "Players in Team:"
+    if len(teamArr) > 0:
+        for player in teamArr:
+            sliced = slicePlayerKey(player)
+            returnValue = returnValue + "\n"
+            returnValue = returnValue + "" + sliced[0] + " | " + sliced[1]
+    else:
+        print("team was 0")
+    
+    print("Returning from list team: \n" + returnValue)
+    return returnValue
+
+def slicePlayerKey(playerKey):
+    index = playerKey.find(",,--")
+    region = playerKey[:index]
+    name = playerKey[index+4:len(playerKey)]
+    return (region, name)
+
+
+def createPlayerKeyInternal(region, name):
+    return region + ",,--" + name
 
 #DISCORD BOT COMMANDS ----------------------------------
 bot = commands.Bot(command_prefix = "!", description = "SoloQ Bot")
@@ -65,12 +108,100 @@ bot = commands.Bot(command_prefix = "!", description = "SoloQ Bot")
 async def on_ready():
 	print("Your soloQ bot is ready for use !")
 
-@bot.command()
-async def soloQ(ctx):
-    await ctx.send("https://tenor.com/view/cops-police-sirens-catching-crminals-what-you-gonna-do-gif-22472645")
-    for i in range(5):
-        df1 = df.loc[[i]]
-        await ctx.send(df1.to_string(header=False, index=False))
-        i = i + 1
+@bot.command(name="gp")
+async def getPlayer(ctx,arg1,arg2):
+    try: 
+        region = arg1
+        playerName = arg2
+        await ctx.send(getSummonerByRegionName(region, playerName))
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
 
-bot.run("INSERT DISCORD BOT TOKEN HERE") # here paste in-between double quotes your discord bot token
+@bot.command(name="mc")
+async def getPlayerMatchCount(ctx,arg1,arg2,arg3):
+    try:
+        region = arg1
+        playerName = arg2
+        timeframe = arg3
+        print(arg1 + arg2 + arg3)
+        summoner = getSummonerByRegionName(region, playerName)
+        print(summoner)
+        print(region)
+        name = getNameBySummonerInternal(summoner)
+        puuId = getPuuidBySummonerInternal(summoner)
+        timestring = getTimeStringFromInput(timeframe)
+
+        timeframe = getTimeByStringInternal(timeframe)
+        #use the region selector for correct api. very simple..
+        region = regionSelectorSmallBig(region)
+        matchlist = getMatchListByRegionAndIdAndTimeFrame(region,puuId, timeframe)
+
+        await ctx.send(name + " has played " + str(len(matchlist)) + "games " + timestring)
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
+
+@bot.command(name="police")
+async def teamCheckup(ctx, timeframe):
+    try:
+        returnValue = ""
+        timestring = getTimeStringFromInput(timeframe)
+        timeframe = getTimeByStringInternal(timeframe)
+        for playerKey in teamArr:
+
+            sliced = slicePlayerKey(playerKey)
+            summoner = getSummonerByRegionName(sliced[0], sliced[1])
+            puuId = getPuuidBySummonerInternal(summoner)
+            mlRegion = regionSelectorSmallBig(sliced[0])
+            matchlist = getMatchListByRegionAndIdAndTimeFrame(mlRegion,puuId,timeframe)
+
+            returnValue = returnValue + "\n"
+            returnValue = returnValue + "Player: " + sliced[0] + " | " + sliced[1] + " has played " + str(len(matchlist)) + "games " + timestring
+        await ctx.send(returnValue)
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
+        
+
+@bot.command(name="lt")
+async def listTeam(ctx):
+    try:
+        await ctx.send(listTeamInternal())
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
+
+
+@bot.command(name="add")
+async def addPlayer(ctx,arg1,arg2):
+    try: 
+        region = arg1
+        name = arg2
+        addPlayerToTeamInternal(region, name)
+        await ctx.send("Player added to team")
+
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
+
+@bot.command(name="rm")
+async def removePlayerFromTeam(ctx, arg1, arg2):
+    try:
+        region = arg1
+        name = arg2
+        removePlayerFromTeamInternal(region, name)
+        await ctx.send("Player Removed")
+    except Exception as e:
+        print(str(e))
+        await ctx.send("Error: ")
+        await ctx.send(str(e))
+
+
+
+bot.run("key2") # here paste in-between double quotes your discord bot token
